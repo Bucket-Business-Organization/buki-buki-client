84781eef7eec46ea69c982995afa85bd9c80dca5
simyuseon <yseon0508@naver.com>
2023-05-04 01:19:55 +0900

    Feat: 프로필 페이지에 컴포넌트 작업 확인

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,9 +1,48 @@
-import ProfileForm from "@/components/common/ProfileForm";
+import InputForm from "@/components/common/InputForm";
 import BaseLayout from "../components/BaseLayout";
+import SearchForm from "@/components/common/SearchForm";
+import UserModal from "@/components/common/UserModal";
+
+import { useState } from "react";
+import AlertModal from "@/components/common/AlertModal";
+
 const Profile = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [showAlertModal, setShowAlertModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleCloseAlertModal = () => {
+    setShowAlertModal(false);
+  };
+
   return (
     <BaseLayout>
-      <ProfileForm />
+      <InputForm />
+      <SearchForm />
+      <button onClick={handleOpenModal}>Open UserModal</button>
+      <br />
+      <button onClick={() => setShowAlertModal(true)}>Open AlertModal</button>
+      <UserModal
+        isOpen={modalOpen}
+        onClose={handleCloseModal}
+        imageSrc="https://i.ibb.co/h7453Mp/image.png"
+        imageAlt="Profile image"
+        userName="다람쥐"
+        userStatusMessage="날 떠나지마"
+        groupName="산오르기"
+        isConfirm={false}
+        showUserInfo={false}
+      />
+      {showAlertModal && (
+        <AlertModal onClose={handleCloseAlertModal} isWithdraw />
+      )}
     </BaseLayout>
   );
 };
