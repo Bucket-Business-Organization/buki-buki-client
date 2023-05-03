0f0c6ec7b95d1a8a6e341d97c0f3dbbe18b10b72
simyuseon <yseon0508@naver.com>
2023-05-04 01:11:59 +0900

    Feat: 기본버튼 컴포넌트 작업

---
+++ components/common/DefaultButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type Props = {
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+  className?: string;
+};
+
+const DefaultButton: React.FC<Props> = ({
+  label,
+  onClick,
+  disabled = false,
+  className = "",
+}) => {
+  return (
+    <button
+      className={`px-4 py-2 rounded font-bold text-white disabled:opacity-50 ${
+        disabled
+          ? "bg-gray-400 cursor-not-allowed"
+          : "bg-emerald-500 hover:bg-emerald-400 cursor-pointer"
+      } ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default DefaultButton;
