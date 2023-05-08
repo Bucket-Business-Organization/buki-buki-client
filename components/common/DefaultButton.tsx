b2a10032a97224b679b1499d453060369d0f2f54
simyuseon <yseon0508@naver.com>
2023-05-08 15:02:46 +0900

    Design: 버튼 컴포넌트 border radius 둥글게 조정

--- components/common/DefaultButton.tsx
+++ components/common/DefaultButton.tsx
@@ -15,7 +15,7 @@ const DefaultButton: React.FC<Props> = ({
 }) => {
   return (
     <button
-      className={`px-4 py-2 rounded font-bold text-white disabled:opacity-50 ${
+      className={`px-4 py-2 rounded-md  text-white disabled:opacity-50 ${
         disabled
           ? "bg-gray-400 cursor-not-allowed"
           : "bg-emerald-500 hover:bg-emerald-400 cursor-pointer"
