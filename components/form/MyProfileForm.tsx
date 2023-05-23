887f1432abefbf52de5ac69380f0c8497793b385
simyuseon <yseon0508@naver.com>
2023-05-23 12:03:24 +0900

    Design: 피그마 시안 변경 및 내정보관리 추가

--- components/form/MyProfileForm.tsx
+++ components/form/MyProfileForm.tsx
@@ -1,160 +1,9 @@
-// import { useState } from "react";
-
-// type ProfileFormProps = {
-//   name: string;
-//   gender: string;
-//   numCompleted: number;
-//   birthday?: Date;
-//   location: string;
-//   onSubmit: (values: ProfileFormValues) => void;
-// };
-
-// type ProfileFormValues = {
-//   name: string;
-//   gender: string;
-//   numCompleted: number;
-//   birthday?: Date;
-//   location: string;
-// };
-
-// const InputForm = ({
-//   name,
-//   gender,
-//   numCompleted,
-//   birthday,
-//   location,
-//   onSubmit,
-// }: ProfileFormProps) => {
-//   const [formValues, setFormValues] = useState<ProfileFormValues>({
-//     name,
-//     gender,
-//     numCompleted,
-//     birthday: birthday ? new Date(birthday) : undefined,
-//     location,
-//   });
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     if (name === "birthday") {
-//       const selectedDate = new Date(value);
-//       const today = new Date();
-//       if (selectedDate > today) {
-//         alert("생년월일은 오늘 이전 날짜만 입력 가능합니다.");
-//         setFormValues((prevValues) => ({
-//           ...prevValues,
-//           birthday: undefined,
-//         }));
-//         return;
-//       }
-//     }
-//     setFormValues((prevValues) => ({
-//       ...prevValues,
-//       [name]: name === "birthday" ? new Date(value) : value,
-//     }));
-//   };
-
-//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const { name, gender, numCompleted, birthday, location } = formValues;
-//     if (!name || !gender || !numCompleted || !birthday || !location) {
-//       alert("모든 항목을 입력해주세요.");
-//       return;
-//     }
-//     onSubmit(formValues);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div className="flex flex-col items-center">
-//         <h1 className="text-2xl font-bold mb-8">프로필 수정</h1>
-//         <div className="w-96 space-y-4">
-//           <div>
-//             <label htmlFor="name" className="sr-only">
-//               Name
-//             </label>
-//             <input
-//               type="text"
-//               name="name"
-//               id="name"
-//               className="p-2 rounded-md border-gray-300 w-full"
-//               placeholder="이름(닉네임)"
-//               value={formValues.name}
-//               onChange={handleChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="gender" className="sr-only">
-//               Gender
-//             </label>
-//             <input
-//               type="text"
-//               name="gender"
-//               id="gender"
-//               className="p-2 rounded-md border-gray-300 w-full"
-//               placeholder="성별"
-//               value={formValues.gender}
-//               onChange={handleChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="numCompleted" className="sr-only">
-//               Number of Completed Tasks
-//             </label>
-//             <input
-//               type="number"
-//               name="numCompleted"
-//               id="numCompleted"
-//               className="p-2 rounded-md border-gray-300 w-full"
-//               placeholder="달성한 버킷리스트 수"
-//               value={formValues.numCompleted}
-//               onChange={handleChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="birthday" className="sr-only">
-//               Birthday
-//             </label>
-//             <input
-//               type="date"
-//               name="birthday"
-//               id="birthday"
-//               className="p-2 rounded-md border-gray-300 w-full"
-//               placeholder="생년월일"
-//               value={formValues.birthday?.toISOString().substr(0, 10)}
-//               onChange={handleChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="location" className="sr-only">
-//               Location
-//             </label>
-//             <input
-//               type="text"
-//               name="location"
-//               id="location"
-//               className="p-2 rounded-md border-gray-300 w-full"
-//               placeholder="주거지역"
-//               value={formValues.location}
-//               onChange={handleChange}
-//             />
-//           </div>
-//         </div>
-//         <button
-//           type="submit"
-//           className="mt-8 bg-emerald-500 hover:bg-emerald-400 text-white font-semibold py-2 px-4 rounded-md"
-//         >
-//           수정완료
-//         </button>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default InputForm;
-
 import React, { useState } from "react";
 import Input from "../common/Input";
 import DefaultButton from "../common/DefaultButton";
+import ProfileImage from "../common/ProfileImage";
+import { BsFillPencilFill } from "react-icons/bs";
+import WithdrawalModal from "../modal/WithdrawalFromMembership";
 
 interface ProfileFormProps {
   nickname: string;
@@ -162,20 +11,28 @@ interface ProfileFormProps {
   bucketListCount: number;
   birthdate: string;
   location: string;
+  password: string;
 }
 
-const ProfileForm: React.FC<ProfileFormProps> = ({
+const MyProfileForm: React.FC<ProfileFormProps> = ({
   nickname,
   gender,
   bucketListCount,
   birthdate,
   location,
+  password,
 }) => {
   const [isEditMode, setIsEditMode] = useState(false);
-  const [updatedNickname, setUpdatedNickname] = useState(nickname);
-  const [updatedGender, setUpdatedGender] = useState(gender);
-  const [updatedBirthdate, setUpdatedBirthdate] = useState(birthdate);
-  const [updatedLocation, setUpdatedLocation] = useState(location);
+  const [isNickNameEditMode, setIsNickNameEditMode] = useState(false);
+  const [isPasswordEditMode, setIsPasswordEditMode] = useState(false);
+  const [isMyInfoMode, setIsMyInfoMode] = useState(false);
+  const [updatedNickname, setUpdatedNickname] = useState("현의 선");
+  const [updatedGender, setUpdatedGender] = useState("여자");
+  const [updatedBirthdate, setUpdatedBirthdate] = useState("1998-04-04");
+  const [updatedPassword, setUpdatedPassword] = useState("12345");
+  const [updatedLocation, setUpdatedLocation] = useState("서울시 송파구");
+  const [withdrawalFromMembership, setWithdrawalFromMembership] =
+    useState(false);
 
   const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUpdatedNickname(event.target.value);
@@ -204,6 +61,15 @@ const ProfileForm: React.FC<ProfileFormProps> = ({
   const handleEditClick = () => {
     setIsEditMode(true);
   };
+  const handleMyInfoClick = () => {
+    setIsMyInfoMode(true);
+  };
+  const handlNickNameEditClick = () => {
+    setIsNickNameEditMode(true);
+  };
+  const handlPasswordEditClick = () => {
+    setIsPasswordEditMode(true);
+  };
 
   const handleSaveClick = () => {
     // Save the updated profile information here
@@ -213,82 +79,301 @@ const ProfileForm: React.FC<ProfileFormProps> = ({
     setUpdatedLocation(updatedLocation);
     setIsEditMode(false);
   };
+  const handleSaveNickNameClick = () => {
+    // Save the updated profile information here
+    setUpdatedNickname(updatedNickname);
+    setIsNickNameEditMode(false);
+  };
+  const handlePaswwordSaveClick = () => {
+    // Save the updated profile information here
+    setUpdatedPassword(updatedPassword);
+    setIsPasswordEditMode(false);
+  };
 
   const handleCancelClick = () => {
     // Reset the updated profile information to the original values here
     setIsEditMode(false);
   };
+  const handleNickCancelClick = () => {
+    // Reset the updated profile information to the original values here
+    setIsNickNameEditMode(false);
+  };
+  const handleNPasswordCancelClick = () => {
+    // Reset the updated profile information to the original values here
+    setIsPasswordEditMode(false);
+  };
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <Input
-        id="nickname"
-        label="Nickname"
-        value={updatedNickname}
-        onChange={handleNicknameChange}
-        showLabel={false}
-        disabled={!isEditMode}
-        defaultValue={updatedNickname}
-        placeholder="이름(닉네임)"
-      />
-      <Input
-        id="gender"
-        label="Gender"
-        value={updatedGender}
-        onChange={handleGenderChange}
-        showLabel={false}
-        disabled={!isEditMode}
-        defaultValue={updatedGender}
-        placeholder="성별"
-      />
-      <Input
-        id="bucketListCount"
-        label="Bucket List Count"
-        type="number"
-        value={bucketListCount !== undefined ? bucketListCount.toString() : ""}
-        onChange={() => {}}
-        showLabel={false}
-        disabled={true}
-        defaultValue={
-          bucketListCount !== undefined ? bucketListCount.toString() : ""
-        }
-        placeholder="달성한 버킷리스트 수"
-      />
+    <div className="rounded-2xl	border-solid	border-gray-200 border p-4 py-1">
+      {/* 내정보 */}
+      {isMyInfoMode ? (
+        <div
+          className={
+            !isMyInfoMode
+              ? "flex  items-center justify-between"
+              : "flex  items-center justify-center"
+          }
+        >
+          <div className="flex items-center justify-center flex-col h-full">
+            <div className="flex items-center justify-center">
+              <ProfileImage src="/다람.jpeg" alt="다람쥐 프로필" />
+            </div>
+            {/* 닉 변경 */}
+            {!isNickNameEditMode ? (
+              <div className="flex items-center justify-center">
+                <div className="text-xl flex items-center justify-center mr-2">
+                  현의 선
+                </div>
+                <BsFillPencilFill
+                  className="text-gray-400"
+                  onClick={handlNickNameEditClick}
+                />
+              </div>
+            ) : (
+              <div className="">
+                <div className="flex">
+                  <div className="text-gray-500 w-20 ">닉네임</div>
 
-      <Input
-        id="birthdate"
-        label="Birthdate"
-        type="date"
-        value={updatedBirthdate}
-        onChange={handleBirthdateChange}
-        showLabel={false}
-        disabled={!isEditMode}
-        defaultValue={updatedBirthdate}
-        placeholder="생년월일"
-      />
-      <Input
-        id="location"
-        label="Location"
-        value={updatedLocation}
-        onChange={handleLocationChange}
-        showLabel={false}
-        disabled={!isEditMode}
-        defaultValue={updatedLocation}
-        placeholder="주거지역"
-      />
-      {isEditMode ? (
-        <>
-          <div className="flex space-x-2">
-            <DefaultButton label="프로필 수정완료" onClick={handleSaveClick} />
-            <DefaultButton label="수정취소" onClick={handleCancelClick} />
+                  <Input
+                    id="닉네임"
+                    label="닉네임"
+                    value={updatedNickname}
+                    onChange={handleNicknameChange}
+                    showLabel={false}
+                    disabled={!isNickNameEditMode}
+                    defaultValue={updatedNickname}
+                    placeholder="이름(닉네임)"
+                  />
+                </div>
+
+                <div className="text-right">
+                  <div className="flex justify-end">
+                    <DefaultButton
+                      label="확인"
+                      onClick={handleSaveNickNameClick}
+                      className="text-sm text-blue-600 bg-white hover:bg-white"
+                    />
+                    <DefaultButton
+                      label="취소"
+                      onClick={handleNickCancelClick}
+                      className="text-sm text-blue-600 bg-white hover:bg-white"
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-        </>
+
+          {isMyInfoMode ? null : (
+            <div>
+              <DefaultButton
+                label={`내 정보 관리 >`}
+                onClick={handleMyInfoClick}
+                className=" text-sm  hover:bg-white hover:text-black text-green-950 bg-white"
+              />
+            </div>
+          )}
+        </div>
       ) : (
-        <DefaultButton label="프로필 수정하기" onClick={handleEditClick} />
+        <div className="">
+          <div className="flex  items-center justify-between">
+            <div className="flex items-center justify-center text-center ">
+              <div className="flex items-center justify-center ml-3">
+                <ProfileImage src={"/다람.jpeg"} alt={"다람쥐 프로필"} />
+              </div>
+              <div className="text-xl ml-10 ">현의 선</div>
+            </div>
+
+            {isMyInfoMode ? null : (
+              <div>
+                <DefaultButton
+                  label={`내 정보 관리 >`}
+                  onClick={handleMyInfoClick}
+                  className=" text-sm  hover:bg-white hover:text-black text-green-950 bg-white"
+                />
+              </div>
+            )}
+          </div>
+          <div className="flex items-start px-2 ">
+            <DefaultButton
+              label={"버킷 보러가기"}
+              onClick={() => {
+                alert("버킷목록");
+              }}
+              className="text-sm	px-1 mr-9"
+            />
+            {!isEditMode ? (
+              <div className="text-base leading-relaxed">
+                <div>{updatedGender}</div>
+                <div>{updatedBirthdate}</div>
+                <div>{updatedLocation}</div>
+              </div>
+            ) : (
+              <div className="">
+                <div className="flex items-center ">
+                  <Input
+                    id="gender"
+                    label="Gender"
+                    value={updatedGender}
+                    onChange={handleGenderChange}
+                    showLabel={false}
+                    disabled={!isEditMode}
+                    defaultValue={updatedGender}
+                    placeholder="성별"
+                  />
+                </div>
+                <div className="flex items-center">
+                  <Input
+                    id="birthdate"
+                    label="Birthdate"
+                    type="date"
+                    value={updatedBirthdate}
+                    onChange={handleBirthdateChange}
+                    showLabel={false}
+                    disabled={!isEditMode}
+                    defaultValue={updatedBirthdate}
+                    placeholder="생년월일"
+                  />
+                </div>
+                <div className="flex items-center">
+                  <Input
+                    id="location"
+                    label="Location"
+                    value={updatedLocation}
+                    onChange={handleLocationChange}
+                    showLabel={false}
+                    disabled={!isEditMode}
+                    defaultValue={updatedLocation}
+                    placeholder="주거지역"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       )}
+
+      {!isMyInfoMode ? (
+        <div className="text-right">
+          {isEditMode ? (
+            <>
+              <div className="flex justify-end">
+                <DefaultButton
+                  label="수정완료"
+                  onClick={handleSaveClick}
+                  className="text-sm text-blue-600 bg-white hover:bg-white"
+                />
+                <DefaultButton
+                  label="수정취소"
+                  onClick={handleCancelClick}
+                  className="text-sm text-blue-600 bg-white hover:bg-white"
+                />
+              </div>
+            </>
+          ) : (
+            <DefaultButton
+              label={"수정하기"}
+              onClick={handleEditClick}
+              className="text-sm text-blue-600 bg-white hover:bg-white"
+            />
+          )}
+        </div>
+      ) : null}
+      {/* 개인정보 비밀번호 변경 */}
+      {isMyInfoMode ? (
+        <div className="mt-10 w-2/3 flex flex-col justify-center m-auto ">
+          <h2 className="text-lg font-bold border-b border-black mb-4">
+            개인정보
+          </h2>
+          <div className="flex ">
+            <div className="text-gray-500 w-20 ">이메일</div>
+
+            <div className="text-black">yseon@naver.com</div>
+          </div>
+          {!isPasswordEditMode ? (
+            <div className="w-full  flex justify-between">
+              <div className="flex ">
+                <div className="text-gray-500 w-20 ">비밀번호</div>
+                <Input
+                  id={"비밀번호"}
+                  label={"비밀번호"}
+                  defaultValue={updatedPassword}
+                  type="password"
+                  showLabel={false}
+                  disabled={!isPasswordEditMode}
+                  className="border-none ml-4"
+                />
+              </div>
+              <DefaultButton
+                label={"변경하기"}
+                onClick={handlPasswordEditClick}
+                className="text-sm text-blue-600 bg-white hover:bg-white"
+              />
+            </div>
+          ) : (
+            <div>
+              <div className="flex mt-4">
+                <h2 className="text-gray-500 w-20">비밀번호</h2>
+                <div className="w-full">
+                  <Input
+                    id={"현재 비밀번호"}
+                    label={"현재 비밀번호"}
+                    type="password"
+                    showLabel={false}
+                    disabled={isPasswordEditMode}
+                    placeholder="현재 비밀번호"
+                  />
+                  <Input
+                    id={"새 비밀번호"}
+                    label={"새 비밀번호"}
+                    type="password"
+                    showLabel={false}
+                    disabled={isPasswordEditMode}
+                    placeholder="새 비밀번호"
+                  />
+                  <Input
+                    id={"새 비밀번호 확인"}
+                    label={"새 비밀번호 확인"}
+                    type="password"
+                    showLabel={false}
+                    disabled={isPasswordEditMode}
+                    placeholder="새 비밀번호 확인"
+                  />
+                </div>
+              </div>
+              <div className="text-right">
+                <div className="flex justify-end">
+                  <DefaultButton
+                    label="확인"
+                    onClick={handlePaswwordSaveClick}
+                    className="text-sm text-blue-600 bg-white hover:bg-white"
+                  />
+                  <DefaultButton
+                    label="취소"
+                    onClick={handleNPasswordCancelClick}
+                    className="text-sm text-blue-600 bg-white hover:bg-white"
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          <div className="flex justify-between items-center border-b border-black my-4">
+            <h2 className="text-lg font-bold  ">회원탈퇴</h2>
+            <button
+              className="mr-5"
+              onClick={() => {
+                setWithdrawalFromMembership(true);
+              }}
+            >{`>`}</button>
+            <WithdrawalModal
+              withdrawalFromMembership={withdrawalFromMembership}
+              setWithdrawalFromMembership={setWithdrawalFromMembership}
+            />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
 
-export default ProfileForm;
+export default MyProfileForm;
